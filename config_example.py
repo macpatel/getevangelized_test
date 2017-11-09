3ae5f06f91791a4f16f74092e8f594c87f54dd56
Mahesh Patel <patel.mahesh034@gmail.com>
2017-11-09 23:05:53 +0530

    config example file added

---
+++ config_example.py
@@ -0,0 +1,10 @@
+CLIENT_ID="dummy"
+CLIENT_SECRET="dummy"
+REDIRECT_URL="dummy"
+INSTA_AUTH_URL = "dummy"
+CLIENT_INSTA_AUTH_URL = "dummy"
+ACCESS_TOKEN_URL="https://api.instagram.com/oauth/access_token"
+INSTAGRAM_URL = "https://www.instagram.com/"
+MONGODB_SETTINGS = {
+	'db' : 'getevangelized'
+}
