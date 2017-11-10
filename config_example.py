443e1be5143d876fe978f3beba4046e7a30eed89
Mahesh Patel <patel.mahesh034@gmail.com>
2017-11-10 10:57:39 +0530

    updated config file

--- config_example.py
+++ config_example.py
@@ -6,5 +6,10 @@ CLIENT_INSTA_AUTH_URL = "dummy"
 ACCESS_TOKEN_URL="https://api.instagram.com/oauth/access_token"
 INSTAGRAM_URL = "https://www.instagram.com/"
 MONGODB_SETTINGS = {
-	'db' : 'getevangelized'
+	'db' : 'getevangelized',
+	# 'host': 'mongodb://localhost/database_name',
+    # 'username':'webapp',
+    # 'password':'pwd123',
+    # 'host': '192.168.1.35',
+    # 'port': 12345	
 }
